bac375115610c6317ba7978fe8762665cbbb1dc7
Kent Fredric <kentfredric@gmail.com>
2015-03-24 03:32:55 +1300

    Remove use of Moose::Autobox

--- lib/Pod/Elemental/Transformer/WikiDoc.pm
+++ lib/Pod/Elemental/Transformer/WikiDoc.pm
@@ -56,8 +56,6 @@ Possibly the most succinct, powerful use case is this:
 
 use namespace::autoclean;
 
-use Moose::Autobox;
-
 use Pod::Elemental::Types qw(FormatName);
 use Pod::WikiDoc;
 
@@ -83,14 +81,16 @@ sub transform_node {
   my ($self, $node) = @_;
   my $children = $node->children;
 
-  PASS: for (my $i = 0; $i < $children->length; $i++) {
+  # Note to future hackers. Don't try replacing this with a simple range operator.
+  # $children gets modified on some loops!
+  PASS: for (my $i = 0; $i < scalar @{ $children }; $i++) {
     my $para = $children->[$i];
     next unless $para->isa('Pod::Elemental::Element::Pod5::Region')
          and    ! $para->is_pod
          and    $para->format_name eq $self->format_name;
 
     confess "wikidoc transformer expects wikidoc region to contain 1 Data para"
-      unless $para->children->length == 1
+      unless scalar @{$para->children} == 1
       and    $para->children->[0]->isa('Pod::Elemental::Element::Pod5::Data');
 
     my $text    = $para->children->[0]->content;
@@ -101,11 +101,11 @@ sub transform_node {
 
     my $new_doc = Pod::Elemental->read_string($new_pod);
     Pod::Elemental::Transformer::Pod5->transform_node($new_doc);
-    $new_doc->children->shift
+    shift @{ $new_doc->children }
       while
       $new_doc->children->[0]->isa('Pod::Elemental::Element::Pod5::Nonpod');
 
-    splice @$children, $i, 1, $new_doc->children->flatten;
+    splice @$children, $i, 1, @{ $new_doc->children };
   }
 
   return $node;
